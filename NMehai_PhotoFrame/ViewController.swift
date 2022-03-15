164ffffc553e54a93eff7075d3db8f9bda7ffa86
Noor Mehai <nmehai@montgomerycollege.edu>
2022-03-15 18:41:36 -0400

    Initial Commit

---
+++ NMehai_PhotoFrame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NMehai_PhotoFrame
+//
+//  Created by Noor Mehai on 2/17/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ NMehai_PhotoFrameUITests/NMehai_PhotoFrameUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  NMehai_PhotoFrameUITestsLaunchTests.swift
+//  NMehai_PhotoFrameUITests
+//
+//  Created by Noor Mehai on 2/17/22.
+//
+
+import XCTest
+
+class NMehai_PhotoFrameUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
